9437ee670c12a7ce07919e85ca7817ed77720c4a
RealPacket <gpgkey.levers657@passinbox.com>
2025-02-13 13:46:37 -0600

    fix(main.js): add more domain aliases and change description

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 // @name         Vape V4
 // @namespace    http://7granddadpgn.github.io
 // @version      2024-07-29
-// @description  A browser script made to give enhancements on miniblox.io
+// @description  A browser script made to give enhancements on Miniblox
 // @author       7GrandDad
 // @match        https://miniblox.io/*
+// @match        https://miniblox.org/*
+// @match        https://miniblox.online/*
+// @match        https://bloxbattles.io/*
 // @icon         https://miniblox.io/favicon.png
 // @grant        GM_getValue
 // @grant        GM_setValue

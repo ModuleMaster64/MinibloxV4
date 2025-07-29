bd435a653b57954e443ed9bf48b93e09a8541eb7
ee6-lang <ee6-lang@noreply.codeberg.org>
2025-07-29 15:27:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MINIBLOX v4
-// @namespace    http://7granddadpgn.github.io
+// @namespace    https://codeberg.org/ee6-lang
 // @version      v3.26-dev
 // @description  A browser script made to give huge enhancements on Miniblox!
 // @author       ModuleM64, 7GrandDadPGN, RealPacket, ProgMEM-CC, he557
